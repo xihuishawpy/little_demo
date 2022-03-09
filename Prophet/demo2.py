2f36059d70c57836162319fd97a9c4eee5f9a25e
shaw <1040310273@qq.com>
2022-03-09 16:04:44 +0800

    add

---
+++ Prophet/demo2.py
@@ -0,0 +1,32 @@
+
+import pandas as pd
+from prophet import Prophet
+
+df = pd.read_csv('https://raw.iqiq.io/facebook/prophet/main/examples/example_wp_log_R_outliers1.csv')
+m = Prophet()
+m.fit(df)
+future = m.make_future_dataframe(periods=1096)
+forecast = m.predict(future)
+fig = m.plot(forecast)
+
+# 1、历史数据有缺失值，影响预测，在prophet中用None填充
+df.loc[(df['ds'] > '2010-01-01') & (df['ds'] < '2011-01-01'), 'y'] = None
+model = Prophet().fit(df)
+fig = model.plot(model.predict(future))
+
+# 2、历史数据有异常值
+df1 = pd.read_csv('https://hk1.monika.love/facebook/prophet/main/examples/example_wp_log_R_outliers2.csv')
+m = Prophet()
+m.fit(df1)
+future = m.make_future_dataframe(periods=1096)
+forecast = m.predict(future)
+fig = m.plot(forecast)
+
+# 将异常部分剔除，用None填充
+df.loc[(df['ds'] > '2015-06-01') & (df['ds'] < '2015-06-30'), 'y'] = None
+m = Prophet().fit(df)
+fig = m.plot(m.predict(future))
+
+
+
+
